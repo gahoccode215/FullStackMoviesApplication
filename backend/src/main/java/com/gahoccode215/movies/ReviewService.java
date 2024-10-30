7d274b08ee580f75d0046eefe7da40a19fd24c47
dominhit215@gmail.com <dominhit215@gmail.com>
2024-10-30 23:48:22 +0700

    movie controller and review service

---
+++ backend/src/main/java/com/gahoccode215/movies/ReviewService.java
@@ -0,0 +1,10 @@
+package com.gahoccode215.movies;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public Review createReview(String reviewBody, String imdbId){
+
+    }
+}

85f264990cdba6ebd4e0fecda55bf9bbbd54f1bf
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-04 03:46:51 +0700

    create Home and Hero components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import api from "./api/axiosConfig";
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import Home from './components/home/Home';
+import { Routes, Route } from 'react-router-dom';
 function App() {
 
   const [movies, setMoives] = useState();
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      setMoives(response.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
 
   return (
     <div className="App">
-      
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+          </Route>
+        </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home({movies}) {
+  return (
+    <div>Welcome!!!</div>
+  )
+}
